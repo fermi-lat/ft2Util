1e0dfe327c373ab5612482f9d0060803cde9b6d7
tramacer <>
2008-02-14 22:15:08 +0000

    added version number to mergeFT2, initialize EarhtCordinate(lat,lon)

--- apps/main.cxx
+++ apps/main.cxx
@@ -54,7 +54,7 @@ int main(int argc, char **argv){
     //std::string FT2_fits_File;
     
 
-    printf("v1r1p31\n");
+    printf("v1r1p33\n");
     
     //--- Get File Names ---
 
--- apps/mergeft2.cxx
+++ apps/mergeft2.cxx
@@ -65,7 +65,7 @@ int main(int iargc, char **argv){
     
     FT2 FT2;
     
-    printf("v1r1p32\n");
+    printf("v1r1p33\n");
     
     //File Handlign
     std::string FT2File;
