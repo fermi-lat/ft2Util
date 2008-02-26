4f21ccc19ae4850fed0665b67091bc45cfdc5f68
tramacer <>
2008-02-26 00:55:49 +0000

    added mainpage.h

---
+++ src/mainpage.h
@@ -0,0 +1,53 @@
+
+/** @mainpage package ft2Util
+* @author Andrea Tramacere <tramacer@slac.stanford.edu>
+*
+* @section intro Introduction
+* Intro
+*
+*
+* <hr>
+* @section makeFT2Entries makeFT2Entries
+*
+* bla bla
+*
+* @subsection jobOptions jobOptions
+* -# --MC
+* -# --Gleam
+* 
+* @param -M7File magic7.txt  
+*
+* @param -FT2_txt_File ft2-out.txt 
+*
+* @param -FT2_fits_File ft2.fits
+*
+* <hr>
+*
+* @section mergeFT2Entries.exe mergeFT2Entries.exe
+*
+* bla bla
+*
+* @subsection jobOptions jobOptions
+*
+* None
+* 
+* @param -FT2_txt_File ft2-out.txt 
+*
+* @param -FT2_fits_File ft2.fits
+*
+*
+* 
+* @section notes release notes
+* release.notes
+*
+* @section requirements requirements
+* @verbinclude requirements
+*
+*/
+
+
+
+
+
+
+
